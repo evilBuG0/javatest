4f444f23c1c577ac6d9543736e09a1d17a7279b1
青梅煮茶 <942943001@qq.com>
2018-05-10 23:24:12 +0800

    20180510

---
+++ src/practice/TestBOX.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class TestBOX {
+	Integer i;
+	int j;
+	public static void main(String[] args) {
+		TestBOX t=new TestBOX();
+		t.go();
+		
+	}
+	public void go() {
+		j=i;
+		System.out.println(j);
+		System.out.println(j);
+	}
+}
